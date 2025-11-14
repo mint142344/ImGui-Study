74d73f595df5b08d9942b7baacffbf095864d925
hzh@XiaoXin <1423443710@qq.com>
2025-11-14 18:29:45 +0800

    添加项目文件。

---
+++ Common/App.cpp
@@ -0,0 +1,115 @@
+﻿#include "App.h"
+#include "imgui_internal.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <Fonts/fa-solid-900.ttf.h>
+#include <Fonts/IconsFontAwesome6.h>
+
+void StyeColorsApp()
+{
+    ImGui::StyleColorsDark();
+    ImPlot::StyleColorsAuto();
+}
+
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+
+App::App(std::string title, int w, int h, int argc, char const* argv[])
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        abort();
+
+    // GL 3.3 + GLSL 330
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);		   // 3.0+ only
+
+    // Create window with graphics context
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const float dpi_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(monitor);
+    Window = glfwCreateWindow(w, h, title.c_str(), NULL, NULL);
+    if (Window == NULL)
+    {
+        fprintf(stderr, "Failed to initialize GLFW window!\n");
+        abort();
+    }
+    glfwMakeContextCurrent(Window);
+    glfwSwapInterval(1);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(Window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(dpi_scale);		 // Scale all sizes by the main_scale.
+    style.FontScaleDpi = dpi_scale;		 // Set initial font scale.
+    ClearColor = ImVec4(0.15f, 0.16f, 0.21f, 1.00f);
+    StyeColorsApp();
+
+    // ImGui 字体
+    ImGuiIO& io = ImGui::GetIO();
+    ImFontConfig font_cfg;
+    font_cfg.OversampleH = font_cfg.OversampleV = 1;
+    font_cfg.PixelSnapH = true;
+    io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\msyh.ttc", 21, &font_cfg);
+    // 合并图标字体
+    static const ImWchar icons_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
+
+    ImFontConfig icons_config;
+    icons_config.MergeMode = true;
+    icons_config.PixelSnapH = true;
+    icons_config.FontDataOwnedByAtlas = false;
+    io.Fonts->AddFontFromMemoryTTF((void*)s_fa_solid_900_ttf, sizeof(s_fa_solid_900_ttf),
+        16.0f, &icons_config, icons_ranges);
+}
+
+App::~App()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(Window);
+    glfwTerminate();
+}
+
+void App::Run()
+{
+    Start();
+    // Main loop
+    while (!glfwWindowShouldClose(Window))
+    {
+        glfwPollEvents();
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        Update();
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(Window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(ClearColor.x, ClearColor.y, ClearColor.z, ClearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(Window);
+    }
+}
+
+ImVec2 App::GetWindowSize() const
+{
+    int w, h;
+    glfwGetWindowSize(Window, &w, &h);
+    return ImVec2(w, h);
+}
